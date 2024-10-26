985f5d495857c15ddf35903a220a96d5936c57b5
Jindrich Kolman <kolman.jindrich@gmail.com>
2024-10-27 01:37:11 +0200

    docker..

--- docker-dev-init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-bun i
-
-rm -rf ./node_modules/yellow-server-common || true
-ln -s ../../../yellow-server-common ./node_modules/
-
-
--- src/start-docker-dev.sh
+++ src/start-docker-dev.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-[ ! -d "./node_modules/" ] && ../docker-dev-init.sh
+[ ! -d "./node_modules/" ] && bun i
+
+rm -rf ./node_modules/yellow-server-common; ln -s ../../../yellow-server-common ./node_modules/yellow-server-common
+
 
 bun server.js --create-database
 bun --watch server.js
