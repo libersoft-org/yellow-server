f55f0139bf8d8b57bf731d5fed57cc14dff06922
Jindrich Kolman <kolman.jindrich@gmail.com>
2024-12-13 14:44:12 +0100

    always bun i

--- src/start-hot.sh
+++ src/start-hot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-[ ! -d "./node_modules/" ] && bun i
+bun i
 echo -ne "\033]0;YELLOW SERVER\007"
 bun --watch server.js
--- src/start-screen.sh
+++ src/start-screen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-[ ! -d "./node_modules/" ] && bun i
+bun i
 screen -dmS yellow-server bash -c '
 echo -ne "\033]0;YELLOW SERVER\007"
 while true; do
