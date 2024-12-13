aa9c509731a0371909edd067ce3f3d2b6f2fae53
Liberland Software Foundation <info@libersoft.org>
2024-12-14 00:23:29 +0100

    hot screen

--- src/start-hot-screen.sh
+++ src/start-hot-screen.sh
@@ -2,4 +2,6 @@
 
 #screen -mS yellow-server bash -c "screen bash& ./start-hot.sh "
 
-screen -dmS yellow-server bash -c "trap bash SIGINT; (./start-hot.sh ; bash);"
+screen -dmS yellow-server bash -c ". ./colors.sh; trap bash SIGINT; (./start-hot.sh ; bash);"
+
+
