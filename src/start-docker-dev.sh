d58b76303b92a85e04f9cacaf12b2622fc4797f8
Your Name <koom@c1>
2025-04-03 17:41:04 +0200

    knex

--- src/start-docker-dev.sh
+++ src/start-docker-dev.sh
@@ -2,7 +2,8 @@
 
 [ ! -d "./node_modules/" ] && ~/.bun/bin/bun i --frozen-lockfile
 rm -rf ./node_modules/yellow-server-common; ln -s ../../../yellow-server-common ./node_modules/yellow-server-common
-~/.bun/bin/bun server.js --create-database
+~/.bun/bin/bun --bun knex migrate:latest --migrations-directory migrations/
+
 while true; do
   ~/.bun/bin/bun --watch server.js
 done
