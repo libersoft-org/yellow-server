fd62ba1c01eefa45ade27bf2619e303b45b4415f
Your Name <koom@c1>
2025-05-13 20:37:30 +0200

    ..

--- start-docker-dev.sh
+++ start-docker-dev.sh
@@ -4,7 +4,6 @@
 rm -rf ./node_modules/yellow-server-common; ln -s ../../yellow-server-common ./node_modules/yellow-server-common
 #~/.bun/bin/bun --bun knex migrate:latest --migrations-directory migrations/
 
-# todo, can remove this once there's knex
 if [ "$CI" = "true" ]; then
   WATCH=""
 else
